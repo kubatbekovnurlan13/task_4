49598bc2e8b9e49d71c1b00d600bc69538fd62e0
Kubatbekov Nurlan <kubatbekovnurlan13@gmail.com>
2022-12-22 06:54:49 +0600

    Fourth commit: changed nexessary methods

--- src/main/java/com/foxminded/task4/cache/Cache.java
+++ src/main/java/com/foxminded/task4/cache/Cache.java
@@ -2,7 +2,6 @@ package com.foxminded.task4.cache;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class Cache {
     private final LinkedHashMap<String, Map<String, Integer>> cashedValues = new LinkedHashMap<>(5) {
@@ -13,21 +12,12 @@ public class Cache {
     };
 
     public boolean checkIfItInCache(String newStringValue) {
-        for (Map.Entry<String, Map<String, Integer>> mapElement : cashedValues.entrySet()) {
-            if (Objects.equals(mapElement.getKey(), newStringValue)) {
-                return true;
-            }
-        }
-        return false;
+        return cashedValues.containsKey(newStringValue);
     }
 
     public void putInCache(String newStringValue, Map<String, Integer> newMapValue) {
         cashedValues.put(newStringValue, newMapValue);
     }
-
-//    public LinkedHashMap<String, Map<String, Integer>> getCashedValues() {
-//        return cashedValues;
-//    }
 
     public Map<String, Integer> getValueFromCashe(String newStringValue) {
         return cashedValues.get(newStringValue);
